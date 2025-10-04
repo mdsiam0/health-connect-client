35419cecf47edcf7963b79c99bde0be507ccd0d5
mdsiam0 <mdsiam5112@gmail.com>
2025-10-04 06:59:28 +0600

    fix social login navigation and  added footer link

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import useAuth from '../hooks/useAuth';
+import { useNavigate } from 'react-router';
+import toast from "react-hot-toast";
 
 const SocialLogin = () => {
-    const {signInWithGoogle} = useAuth();
-    const handleGoogleSignIn = () =>{
-        signInWithGoogle()
-        .then(result =>{
-            console.log(result.user)
-        })
-        .catch(error =>{
-            console.error(error);
-        })
-    }
-    return (
-        <div className='mt-3 text-center '> 
-            <button onClick={handleGoogleSignIn} className="btn btn-outline btn-google w-full">
+  const { signInWithGoogle } = useAuth();
+  const navigate = useNavigate();
 
-                Continue with Google
+  const handleGoogleSignIn = () => {
+    toast.loading("Signing in with Google...");
+    signInWithGoogle()
+      .then((result) => {
+        toast.dismiss();
+        toast.success("Logged in successfully!");
+        console.log(result.user);
+
+        
+        navigate("/");
+      })
+      .catch((error) => {
+        toast.dismiss();
+        console.error(error);
+        toast.error(error.message);
+      });
+  };
+
+  return (
+    <div className='mt-3 text-center'> 
+      <button 
+        onClick={handleGoogleSignIn} 
+        className="btn btn-outline btn-google w-full"
+      >
+        Continue with Google
       </button>
-           
-        </div>
-    );
+    </div>
+  );
 };
 
 export default SocialLogin;
